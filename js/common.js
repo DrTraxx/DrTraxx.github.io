610b76b2cd21335fb69a4d1fa75a887817b4924c
Patrick <66879705+DrTraxx@users.noreply.github.com>
2020-09-24 22:34:45 +0200

    create apis.js update common.js

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
-console.log("Allgemeine JS-Commands");
+function singleBuilding(id) {
+  var returnValue = {};
+  $.getJSON("/api/buildings" + id, function(data) {
+    returnValue = data;
+  });
+  return returnValue;
+}
+
+function singleVehicle(id) {
+  var returnValue = {};
+  $.getJSON("/api/vehicles" + id, function(data) {
+    returnValue = data;
+  });
+  return returnValue;
+}
