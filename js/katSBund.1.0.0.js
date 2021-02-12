5d3ded1bbc065d7eee453330bf4e21e5de12324c
Patrick <66879705+DrTraxx@users.noreply.github.com>
2021-02-12 08:35:39 +0100

    update

--- js/katSBund.1.0.0.js
+++ js/katSBund.1.0.0.js
@@ -1,5 +1,9 @@
 var katSBund = {};
 
-$.get("/alliance_posts/1725151/edit", function(data) {
-  katSBund = JSON.parse($("#alliance_post_content", data).text());
-});
+async function getKatSBundData() {
+  await $.get("/alliance_posts/1725151/edit", function(data) {
+    katSBund = JSON.parse($("#alliance_post_content", data).text());
+  });
+}
+
+getKatSBundData();
