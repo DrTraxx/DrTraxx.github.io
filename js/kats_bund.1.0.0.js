e176a34f15a6e63a4d96d00cd3bbef7f9769f3cd
Patrick <66879705+DrTraxx@users.noreply.github.com>
2021-02-18 18:28:00 +0100

    bugfix

--- js/kats_bund.1.0.0.js
+++ js/kats_bund.1.0.0.js
@@ -1,5 +1,5 @@
 //katSBund
-async function getkatSBund() {
+async function getKatSBund() {
   if(!localStorage.katSBund || JSON.parse(localStorage.katSBund).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) {
     await $.get("/alliance_posts/1725151/edit", function(data) {
       localStorage.setItem("katSBund", JSON.stringify(lastUpdate: new Date().getTime(), value: JSON.parse($("#alliance_post_content", data).text().trim()));
