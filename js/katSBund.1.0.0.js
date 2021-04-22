f465c893e1511dfe0806f5db57278e3154e04e1c
DrTraxx <66879705+DrTraxx@users.noreply.github.com>
2021-04-22 06:32:00 +0200

    Delete katSBund.1.0.0.js

--- js/katSBund.1.0.0.js
+++
@@ -1,9 +0,0 @@
-//katSBund
-async function getKatSBund() {
-  if(!localStorage.katSBund || JSON.parse(localStorage.katSBund).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) {
-    await $.get("/alliance_posts/1725151/edit", function(data) {
-      localStorage.setItem("katSBund", JSON.stringify({lastUpdate: new Date().getTime(), value: JSON.parse($("#alliance_post_content", data).text().trim())}));
-    });
-  }
-  return JSON.parse(localStorage.katSBund).value;
-}
